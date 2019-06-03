5013550ef46a56b48a9660d9bd3bc511f49bbf95
Edward Fernandez <edwardfer.96@gmail.com>
2019-06-03 19:40:58 +0200

    Basic project structure and common classes Camera and Path

---
+++ signdex/common.py
@@ -0,0 +1,25 @@
+import os
+
+import cv2
+
+class Camera:
+    def __init__(self):
+        pass
+        
+    def open(self, source=0):
+        self.source = cv2.VideoCapture(source)
+        
+        if(self.source is None or not self.source.isOpened()):
+            raise Warning('no camera found')
+
+    def read(self):
+        _, frame = self.source.read()
+        
+        return frame
+    
+    def close(self):
+        self.source.release()
+        cv2.destroyAllWindows()
+
+class Path:
+    DATASETS = os.path.join(os.getcwd(), 'datasets')
