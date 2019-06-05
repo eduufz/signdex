39f024dae229c8dbd8a6deda408b18309076ba2a
Edward Fernandez <edwardfer.96@gmail.com>
2019-06-05 17:35:20 +0200

    Methods to load an existing model and to verify existence of a model

--- signdex/modeling.py
+++ signdex/modeling.py
@@ -1,3 +1,32 @@
+# Python
+import os
+# Downloaded
+import tensorflow as tf
+# SignDex
+from signdex.data import Dataset
+from signdex.processing import Processor
+from signdex.common import Path
 
-class Model():
-    pass
+class Model:
+    def __init__(self, name):
+        self.name = name
+        self.path = os.path.join(Path.MODELS, self.name)
+        self.__model = None
+        self.load()
+
+    def load(self):
+        if self.__exists():
+            # Read json model
+            json_model_path = os.path.join(self.path, '{}.json'.format(self.name))
+            json_file = open(json_model_path, 'r')
+            json_model = json_file.read()
+            json_file.close()
+
+            # Load model from json
+            self.__model = tf.keras.models.model_from_json(json_model)
+    
+    def __exists(self):
+        model_list = Path.list_subdirectories(Path.MODELS)
+
+        if self.name in model_list: return True
+        else: return False
